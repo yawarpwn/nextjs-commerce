c3c9eaa71171d798ba033b0617ea95dc742693f7
yawarpwn <john.kazz@gmail.com>
2023-04-19 16:33:22 -0500

    swiper added

---
+++ src/components/HeroSlider.js
@@ -0,0 +1,48 @@
+import { SwiperSlide, Swiper } from "swiper/react"
+import { Pagination, Scrollbar } from "swiper"
+import Image from "next/image"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+export default function HeroSlider() {
+  const banners = [
+    {
+      src: "/assets/banner-cascos-1.webp",
+      alt: "banner de casco para motos 1 ",
+    },
+    {
+      src: "/assets/banner-cascos-2.webp",
+      alt: "banner de casco para motos 2 ",
+    },
+    {
+      src: "/assets/banner-cascos-3.webp",
+      alt: "banner de casco para motos 3 ",
+    },
+    {
+      src: "/assets/banner-cascos-4.webp",
+      alt: "banner de casco para motos 4 ",
+    },
+  ]
+  return (
+    <Swiper
+      id="hero-slider"
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {banners.map(({ src, alt }, index) => (
+        <SwiperSlide key={index}>
+          <div className="w-full h-[200px] md:h-[400px] lg:h-[500px]">
+            <img src={src} alt={alt} className="object-cover h-full w-full" />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import  HeroSlider  from '@/components/HeroSlider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <img src='/assets/banner-motos-1.webp' alt='banner de motos' />
-      Start
+      <HeroSlider />
     </div>
   )
 }
